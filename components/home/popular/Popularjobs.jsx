77b8ad0e0f3ce7c22778c71fa139d8e936be8cdd
maybb2003 <maybb2003@gmail.com>
2023-04-23 19:52:02 +0300

    add axsios- dosent working

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -6,11 +6,17 @@ import { useRouter } from 'expo-router';
 import styles from './popularjobs.style';
 import { COLORS, SIZES } from '../../../constants';
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import { useFetch } from '../../../hook/useFetch';
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+
+  const { data, isLoading, error } = useFetch('search', {
+    query: 'React developer',
+    num_pages: 1
+  })
+
+  console.log(data);
 
 
   return (
@@ -28,8 +34,6 @@ const Popularjobs = () => {
         ): error ? (
           <Text>Somthing went wrong</Text>
         ): (
-          //console.log(<PopularJobCard item={1} />)
-          
             <FlatList
               data={[1,2,3,4]}
               renderItem={({ item }) => (
---
+++ hook/useFetch.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import { query } from "express";
+
+
+
+const useFetch = (endpoint) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+          'content-type': 'application/octet-stream',
+          'X-RapidAPI-Key': 'da0e6e39b6msh92cb39b26c65cb2p10699ejsn2aafab6c6772',
+          'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
+        params: { ...query },
+      };
+
+      const fetchData = async () => {
+        setIsLoading(true);
+
+        try{
+            const res = await axios.request(options);
+            setData(res.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            alert('There is an error')
+        } finally {
+            setIsLoading(false);
+        }
+      }
+
+      useEffect(() => {
+        fetchData();
+      }, []);
+
+      const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+      }
+
+      return{ data, isLoading, error, refetch };
+}
+
+export default useFetch;
